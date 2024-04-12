316c461f21111706e5d9b799416d006a354523d8
{DTBissy} <{isaactheedwards@gmail.com}>
2024-04-12 18:44:16 -0500

    ss

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -8,5 +8,6 @@ void binary_tree_delete(binary_tree_t *tree)
   binary_tree_delete(tree->left);
   binary_tree_delete(tree->right);
 
+  printf("\n Deleting every node: %d", tree->n);
   free(tree);
 }
